810bbc6c003eb3406bc9ba86bea9dfa05bda247e
hygorp <hygor.henrique@live.com>
2024-08-28 10:43:39 -0300

    Initializing Project

---
+++ src/main/java/org/hygorp/bookmarketplace/BookMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package org.hygorp.bookmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hygorp/bookmarketplace/BookMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hygorp.bookmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
