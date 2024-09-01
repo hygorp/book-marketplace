f5441c585c2f6b32141fcd9e3c8a059eeb37117a
hygorp <hygor.henrique@live.com>
2024-09-01 13:53:43 -0300

    Spring Security added to project and disabled for tests

---
+++ src/test/java/org/hygorp/bookmarketplace/configurations/SecurityConfigurationForTests.java
@@ -0,0 +1,24 @@
+package org.hygorp.bookmarketplace.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurationForTests {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()).build();
+    }
+}
